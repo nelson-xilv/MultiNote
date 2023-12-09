a9a8f7a419d7a424fac9f0d66c568555462e0c15
nelson-XILV <khojugken@gmail.com>
2023-12-09 19:29:00 +0300

    feat: adding necessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.androidKsp)
 }
 
 android {
@@ -28,11 +28,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
@@ -45,6 +45,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.circle.imageview)
     implementation(libs.bundles.coroutines)
+    implementation(libs.bundles.room)
+    ksp(libs.room.compiler)
+    implementation(libs.bundles.navigation)
+    implementation(libs.bundles.retrofit)
 
     testImplementation(libs.junit)
 
